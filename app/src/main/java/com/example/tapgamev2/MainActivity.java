a4efd8ed80aa684ee8d4b98d5d586e1318924d9c
marvel-natanael <63591046+marvel-natanael@users.noreply.github.com>
2020-12-16 13:23:11 +0700

    a

--- app/src/main/java/com/example/tapgamev2/MainActivity.java
+++ app/src/main/java/com/example/tapgamev2/MainActivity.java
@@ -2,17 +2,13 @@ package com.example.tapgamev2;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.content.ContextCompat;
-import androidx.core.view.ViewCompat;
 
-import android.content.Context;
 import android.content.SharedPreferences;
-import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.os.CountDownTimer;
 import android.os.Handler;
 import android.view.View;
-import android.widget.Button;
-import android.widget.ImageView;
+import android.widget.ImageButton;
 import android.widget.TextView;
 
 import java.util.Random;
@@ -20,9 +16,9 @@ import java.util.Random;
 public class MainActivity extends AppCompatActivity {
 
     View screenView;
-    Button clickMe;
+    ImageButton clickMe;
     int[] images;
-
+    int[] planet;
     TextView tv_result, tv_info, tv_lose, tv_taps;
 
     int currentTaps = 0;
@@ -43,11 +39,12 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        images = new int[]{R.drawable.a, R.drawable.b, R.drawable.c, R.drawable.d, R.drawable.e, R.drawable.f, R.drawable.g, R.drawable.h};
-
+        images = new int[]{R.drawable.a, R.drawable.b, R.drawable.c, R.drawable.d, R.drawable.f, R.drawable.g, R.drawable.h};
+        planet = new int[]{R.drawable.planet1, R.drawable.planet2};
         screenView = findViewById(R.id.rview);
-        clickMe = (Button) findViewById(R.id.button);
-
+        //screenView.setBackgroundResource(R.drawable.background);
+        clickMe = (ImageButton) findViewById(R.id.button);
+        //clickMe.setImageResource(R.drawable.planet1);
         tv_result = findViewById(R.id.tv_result);
         tv_info = findViewById(R.id.tv_info);
         tv_lose = findViewById(R.id.tv_lose);
@@ -72,7 +69,11 @@ public class MainActivity extends AppCompatActivity {
                     {
                         int aryLength = images.length;
                         Random random = new Random();
-                        int rNum = random.nextInt(aryLength);
+                        final int rNum = random.nextInt(aryLength);
+                        int imgNo=(int) (Math.random() * 2 + 1);
+                        int imgID = getResources().getIdentifier("planet"+ imgNo, "drawable", getPackageName());
+                        clickMe.setImageResource(imgID);
+                        clickMe.requestLayout();
                         screenView.setBackground(ContextCompat.getDrawable(getApplicationContext(), images[rNum]));
                         checkPoint+=2;
                         tapsToWin*=checkPoint;
